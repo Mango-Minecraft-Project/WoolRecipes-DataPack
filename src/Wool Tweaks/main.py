cebbb2a32cac426b278863c6d4e41dd0010f275a
Mango <evan940415@gmail.com>
2022-09-15 06:39:56 +0800

    add files

---
+++ src/Wool Tweaks/main.py
@@ -0,0 +1,12 @@
+from json import dump
+
+def dump_(path, data):
+    with open("./data/wool_tweaks/recipes/" + path + ".json", mode="w") as file:
+        dump(data, file, indent=2)
+
+dump_("wool_to_string", {"type":"crafting_shapeless","ingredients":[{"tag":"wool_tweaks:wt_wool"}],"result":{"item":"minecraft:string","count":4}})
+
+for color in {"red", "black", "blue", "brown", "cyan", "gray", "green", "light_blue", "light_gray", "lime", "magenta", "orange", "pink", "purple", "white", "yellow"}:
+    dump_(f"{color}_bed", {"type":"crafting_shapeless","group":"color_bed","ingredients":[{"tag":"wool_tweaks:wt_bed"},{"item":f"minecraft:{color}_dye"}],"result":{"item":f"minecraft:{color}_bed"}})
+    dump_(f"{color}_carpet", {"type":"crafting_shaped","group":"color_carpet","pattern":["***","*_*","***"],"key":{"*":{"tag":"wool_tweaks:wt_carpet"},"_":{"item":f"minecraft:{color}_dye"}},"result":{"item":f"minecraft:{color}_carpet"}})
+    dump_(f"{color}_wool", {"type":"crafting_shapeless","group":"color_wool","ingredients":[{"tag":"wool_tweaks:wt_wool"},{"item":f"minecraft:{color}_dye"}],"result":{"item":f"minecraft:{color}_wool"}})
